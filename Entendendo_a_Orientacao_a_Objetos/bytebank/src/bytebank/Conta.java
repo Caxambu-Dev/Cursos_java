c004bdb8b038bd2eab9fb27b02d0234488cd0ef4
Caxambu-Dev <caxambudev@gmail.com>
2022-08-21 06:48:33 -0300

    Vinculando classe Cliente à classe Conta

--- Entendendo_a_Orientacao_a_Objetos/bytebank/src/bytebank/Conta.java
+++ Entendendo_a_Orientacao_a_Objetos/bytebank/src/bytebank/Conta.java
@@ -4,7 +4,7 @@ public class Conta {
 	double saldo;
 	int agencia;
 	int numero;
-	String Titular;
+	Cliente Titular;
 
 	public void deposita(double valor) {
 		this.saldo += valor;
--- Entendendo_a_Orientacao_a_Objetos/bytebank/src/bytebank/CriaConta.java
+++ Entendendo_a_Orientacao_a_Objetos/bytebank/src/bytebank/CriaConta.java
@@ -4,25 +4,13 @@ public class CriaConta {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Conta primeiraconta = new Conta();
-		primeiraconta.saldo = 300;
-		//System.out.println(primeiraconta.saldo);
-
-		primeiraconta.saldo += 100;
-		//System.out.println(primeiraconta.saldo);
+		Conta contaDoDaniel = new Conta();
+		contaDoDaniel.Titular = new Cliente();
+		contaDoDaniel.Titular.nome = "Daniel";
 		
-		Conta segundaConta = new Conta();
-		segundaConta.saldo = 500;
 		
-		System.out.println("A primeira conta tem " + primeiraconta.saldo);
-		System.out.println("A segunda conta tem " + segundaConta.saldo);
 		
-		primeiraconta.saca(100);
-		System.out.println("saldo após saque (primeira conta) " + primeiraconta.saldo);
 		
-		primeiraconta.transfere(100,primeiraconta,segundaConta);
-		System.out.println("Após as operações, a primeira conta tem " + primeiraconta.saldo);
-		System.out.println("Após as operações, a segunda conta tem " + segundaConta.saldo);
 		
 
 	}
