5359e65897caf4ce471e80dfc669d66349780199
Caxambu-Dev <caxambudev@gmail.com>
2022-08-23 07:27:51 -0300

    Adição do método 'registra' na classe 'ControleBonificacao'

--- 0003-Java_Polimorfismo_entenda_heranca_e_interfaces/bytebank-herdado/src/ControleBonificacao.java
+++ 0003-Java_Polimorfismo_entenda_heranca_e_interfaces/bytebank-herdado/src/ControleBonificacao.java
@@ -1,4 +1,17 @@
 
 public class ControleBonificacao {
+	private double soma;
+	
+	public void registra(Funcionario f) {
+		double bonificacao = f.getBonificacao();
+		this.soma += bonificacao;
+	}
+
+
+	public double getSoma() {
+		return this.soma;
+	}
+	
+	
 
 }
