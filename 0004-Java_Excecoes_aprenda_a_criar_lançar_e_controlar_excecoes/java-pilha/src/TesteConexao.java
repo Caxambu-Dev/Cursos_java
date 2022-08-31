43d4fc741c360ee3f7d2c49cfa1dac4c09e74b17
Caxambu-Dev <caxambudev@gmail.com>
2022-08-31 08:17:12 -0300

    Refatorando Conexao e TesteConexao

--- 0004-Java_Excecoes_aprenda_a_criar_lançar_e_controlar_excecoes/java-pilha/src/TesteConexao.java
+++ 0004-Java_Excecoes_aprenda_a_criar_lançar_e_controlar_excecoes/java-pilha/src/TesteConexao.java
@@ -3,14 +3,10 @@ public class TesteConexao {
     public static void main(String[] args) {
     	
     	Conexao con = null;
-    	try{
-    	    con = new Conexao();
-    	    con.leDados();
-    	    con.fecha();
+    	try(Conexao conexao = new Conexao() ){
+    	    conexao.leDados();
     	} catch(IllegalStateException ex){
-    	    System.out.println("Deu erro na conexão");
-    	} finally {
-    	    con.fecha();
+    	    System.out.println("Deu erro na conexão");    	
     	}
 	}
 }
